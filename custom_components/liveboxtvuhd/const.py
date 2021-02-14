86ac3593e0338f849f956a637355551c68ba9519
Cédric HUMBERT <cedric.humbert@unattend.fr>
2021-02-14 22:55:21 +0100

    component > integration

--- custom_components/liveboxtvuhd/const.py
+++ custom_components/liveboxtvuhd/const.py
@@ -8,7 +8,7 @@ STARTUP = """
 -------------------------------------------------------------------
 {}
 Version: {}
-This is a custom component
+This is a custom integration.
 If you have any issues with this you need to open an issue here:
 {}
 -------------------------------------------------------------------
